4878883d576f89b8d39931234c246eb439402f90
Mykhailo Nohas <myhazbo@gmail.com>
2023-02-14 23:04:33 +0100

    Const init

---
+++ Battleships.Core/Const.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships.Core
+{
+  public class Const
+  {
+    public static string NotHit = "_ ";
+    public static string Missed = "O ";
+    public static string Injured = "* ";
+    public static string Destroyed = "X ";
+
+    public static char[] Letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+    public static short ColsAmount = 10;
+  }
+}
